2bc77658b3ba30d878024858be72e68c30db4b22
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-22 16:41:24 -0600

    Add script for dropdown and icon to open and close

---
+++ scripts/dropdown.js
@@ -0,0 +1,16 @@
+const open = document.querySelector('.open');
+const navbarSections = document.querySelector('.navbar');
+const close = document.querySelector('.close')
+/* console.log(open); */
+
+function showDropdown() {
+    navbarSections.style.top = '0';
+}
+
+function hideDropdown() {
+    navbarSections.style.top = '-100%';
+}
+
+open.addEventListener('click', showDropdown);
+
+close.addEventListener('click', hideDropdown);
